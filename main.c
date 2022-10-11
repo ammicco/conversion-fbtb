133e3d6b80c321c02ab7f541431f30b126a39b1d
ammicco1 <ngrecchi1@gmail.com>
2022-10-11 14:42:21 +0200

    add base arror hanlder

--- main.c
+++ main.c
@@ -11,9 +11,22 @@ static void math_err(void){
     printf("Math error! Your number isn't in that base.\n");
 }
 
+static void base_error(void){
+    printf("Can't use that base.\n");
+}
+
 int main(int argc, char **argv){
     if(argc < 4){
         usage();
+
+        return -1;
+    }
+
+    if(atoi(argv[2]) == 1 || atoi(argv[2]) == 0 
+    || atoi(argv[3]) == 1 || atoi(argv[3]) == 0
+    || atoi(argv[2]) > 10 || atoi(argv[2]) > 10 
+    || atoi(argv[3]) > 10 || atoi(argv[3]) > 10){
+        base_error();
 
         return -1;
     }
